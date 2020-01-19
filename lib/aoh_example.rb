721bcb985126bfe786c2e9c7b21d992fbd670497
Phoenix R <phoenixrabang@gmail.com>
2020-01-19 02:53:43 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -8,9 +8,9 @@ BRUCE_G = { name:  "Bruce Green", occupation: "Fan of Mildred" }
 
 def assembled_aoh
   new_array = [
-    { :name=> "Don Gately", occupation: "Live-in Staff"}
-    { :name=>  "Joelle van Dyne", occupation: "Radio Personality" }
-    { :name=> "Pat Monteseian", occupation: "Staff" }
+    { :name=> "Don Gately", occupation: "Live-in Staff" },
+    { :name=>  "Joelle van Dyne", occupation: "Radio Personality" },
+    { :name=> "Pat Monteseian", occupation: "Staff" },
     { :name=>  "Kate Gompert", occupation: "None" }
     { :name>  "Bruce Green", occupation: "Fan of Mildred" }
     ]
@@ -19,10 +19,10 @@ end
 
 def literal_aoh
     new_array = [
-    { :name=> "Don Gately", occupation: "Live-in Staff"}
-    { :name=>  "Joelle van Dyne", occupation: "Radio Personality" }
-    { :name=> "Pat Monteseian", occupation: "Staff" }
-    { :name=>  "Kate Gompert", occupation: "None" }
+    { :name=> "Don Gately", occupation: "Live-in Staff"},
+    { :name=>  "Joelle van Dyne", occupation: "Radio Personality" },
+    { :name=> "Pat Monteseian", occupation: "Staff" },
+    { :name=>  "Kate Gompert", occupation: "None" },
     { :name>  "Bruce Green", occupation: "Fan of Mildred" }
     ]
     new_array
